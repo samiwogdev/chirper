a7c48d5109ddb004673f8ddecbc60aff1d087006
Samuel Idowu Ogunbodede <samiwogdev@gmail.com>
2023-04-23 10:32:21 +0100

    add route, configure controller and present blade view

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ChirpController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,6 +23,11 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+//Defined 
+Route::resource('chirps', ChirpController::class)
+->only(['index', 'store'])
+    ->middleware(['auth', 'verified']);
+    
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
